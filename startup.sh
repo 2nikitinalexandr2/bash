21e1b0f1c53c80016f92d792756a2c63e500be5c
Sasha Nikitin <2nikitinalexandr2@gmail.com>
2026-02-01 23:09:04 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -32,7 +32,7 @@
               fastcgi_split_path_info ^(.+.php)(/.+)$;
               fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
               fastcgi_index index.php;
-              fastcgi_param SCRIPT_FILENAME $document_root\$fastcgi_script_name;
+              fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
               include fastcgi_params;
           }
       }
